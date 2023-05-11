9c480aedd73e7f3ea78e811351bebee8af131e8d
EmberTSeal <trishaseal49@gmail.com>
2023-05-11 22:20:55 +0530

    Fix multi-worded inputs and continuing loop at invalid inputs.

--- main.cpp
+++ main.cpp
@@ -6,20 +6,22 @@
 using namespace std;
 
 int main(){
-    int choice;
+    int ch;
+    string choice;
     movies movie_list;
     string name, movie_rate;
     int user_rate, watched;
     while(1){
         cout << "Movie Manager:-" << endl;
         cout << "<1> Add a movie \n<2> Increase watch count \n<3> Display all movies \n<4> Exit \n\n";
-        cout << "Enter your choice: " ;
-        cin >> choice;
-        switch(choice){
+        cout << "Enter your choice: "; 
+        cin >> ch;
+        cin.ignore();
+        switch(ch){
             case 1: cout << "Enter movie name: ";
-                    cin >> name;
+                    getline(cin, name);
                     cout << "Enter movie rating: ";
-                    cin >> movie_rate;
+                    getline(cin, movie_rate);
                     cout << "Enter watch count: ";
                     cin >> watched; 
                     cout << "Enter your rating: ";
@@ -27,13 +29,14 @@ int main(){
                     movie_list.add_movie(name, movie_rate, user_rate, watched);
                     continue;
             case 2: cout << "Enter movie name: ";
-                    cin >> name;
+                    getline(cin, name);
                     movie_list.inc_watch_count(name);
                     continue;
             case 3: movie_list.display_list();
                     continue;
             case 4: break;
             default: cout << "Invalid input!\n";
+                    continue;
         }
         break;
     }
